a720bbcc43165c8bf65b41c7125a3b73a4deb69a
Amar <amar1998mishra@gmail.com>
2023-01-03 15:31:33 +0530

    Add Crud App

---
+++ script.js
@@ -0,0 +1,52 @@
+function onFormSubmit(){
+    event.preventDefault()
+    readUserData()
+
+    // console.log("Running.......")
+}
+
+
+// Read User Data
+
+// function readUserData(){
+    // console.log("Read User Data Fun")
+    // var formData = {}
+    // formData['Name'] = document.getElementById('fname')
+
+
+// }
+
+// let a = document.getElementById('fname')
+// console.log(a)
+
+// let obj1 = {Name : "Prashant", Age : 25}
+// console.log(obj1)
+
+// let obj = {}
+// console.log(obj)
+// objectName['property'] = "Value"
+// obj['Name'] = "Ajay"
+// obj['Age'] = 25
+// console.log(obj)
+
+
+
+// function addNum(){
+//     var a = 20, b = 30;
+//     console.log(a+b)
+// }
+
+// addNum()
+
+
+
+/*
+1. Loop
+2. Function
+3. Array
+4. Call blind Apply
+5. eventListener
+6. CRUD
+7. Fetch
+8. DOM
+*/
